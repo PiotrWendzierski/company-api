f337c8003f19f3c92556e5ca2869e46b5916351a
PiotrWendzierski <piotrwendzierski@gmail.com>
2025-06-01 16:34:47 +0200

    13. Final. Adding API token using Sanctum

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,6 @@ use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\EmployeeController;
 
 Route::middleware('api')->group(function () {
-    Route::apiResource('companies', CompanyController::class);
-    Route::apiResource('employees', EmployeeController::class);
+    Route::apiResource('companies', CompanyController::class)->middleware('auth:sanctum');
+    Route::apiResource('employees', EmployeeController::class)->middleware('auth:sanctum');
 });
